9ab39efc09d28014e845a404271b60173f7cbbbb
yeti1o3 <sanskarsharma987@gmail.com>
2024-07-07 16:00:20 +0530

    changes saved

--- api/index.js
+++ api/index.js
@@ -3,24 +3,34 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import connectDB from './config/db.js';
 import User from './models/User.models.js';
+import bcrypt from 'bcrypt';
 const app=express();
 const port=4000;
 app.use(cors());
 connectDB();
 app.use(bodyParser.json());
+const salt=bcrypt.genSaltSync(10);
 
 app.post('/register', async(req, res) => {
   const {username,password}=req.body;
-    console.log("this is working");
-    if(username && password)
-    {
-      try {const userDoc=await User.create({username,password})
-      console.log("username ",username,"password ",password);
-    res.send(userDoc);}catch{
-      res.send(error);
-    }}
-    else{
-      res.send("error occurred");
+    if(!username||!password)
+      {
+        return res.send("username and password required");
+      }
+      try {
+        const userDoc = await User.create({
+          username,
+          password :bcrypt.hashSync(password,salt)
+        });
+        res.json(userDoc);
+    } catch (error) {
+        if (error.code === 11000) {
+            // Duplicate key error
+            res.status(400).json({ error: "Username already exists" });
+        } else {
+            console.error('Error creating user', error);
+            res.status(500).json({ error: "Internal server error" });
+        }
     }
     }
 );
