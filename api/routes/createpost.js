b13fe8fa27cc2f004a8ad0f5026183dc052f189f
yeti1o3 <sanskarsharma987@gmail.com>
2024-07-09 15:24:46 +0530

    photo upload successful

--- api/routes/createpost.js
+++ api/routes/createpost.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import multer from 'multer';
+import uploadToCloudinary from '../middleware/uploadToCloudinary.js';
+const uploadmiddleware=multer({dest:'uploads/'});
 const router=express.Router();
-router.post('/createpost',async(req,res)=>{
-    console.log(req.body);
-    res.json("ok");
+router.post('/createpost',uploadmiddleware.single('files'),uploadToCloudinary,(req,res)=>{
+    const cloudinaryUrl=req.file.cloudinaryUrl;
+    res.json({msg:'file uploded successfully',url:cloudinaryUrl});
 })
 export default router;
